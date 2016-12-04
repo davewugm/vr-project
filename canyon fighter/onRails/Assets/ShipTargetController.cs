16a9bde7b7c3f05b4aa59069eee9133b3884dac1
davewulumos <dwu@lumosity.com>
2016-12-04 09:22:29 -0800

    update

---
+++ canyon fighter/onRails/Assets/ShipTargetController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipTargetController : MonoBehaviour {
+
+	public GameObject Explosion;
+	public int targetId;
+
+	// Use this for initializationr
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void TargetHit(int damage) {
+		Debug.Log ("CONTROLLER::TARGET HIT " + damage);
+
+		if (damage == 1) {
+			Instantiate (Explosion, this.transform.position, Quaternion.identity);
+			MothershipController2.Instance.RegisterDestroyed (targetId);
+		}
+
+	}
+}
